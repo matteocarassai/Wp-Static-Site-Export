131683dcf94ea45213342935dc8aad2074f79de3
Matteo Carassai <152091181+matteocarassai@users.noreply.github.com>
2025-05-05 16:08:10 +0200

    Add files via upload

--- wp-static-exporter/wp-static-exporter.php
+++ wp-static-exporter/wp-static-exporter.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Plugin Name:       WP Static Exporter
- * Plugin URI:        https://example.com/plugins/the-basics/
+ * Plugin URI:        https://github.com/matteocarassai/Wp-Static-Site-Export/tree/master/wp-static-exporter
  * Description:       Exports WordPress pages and posts into a static HTML/CSS/JS website zip archive.
- * Version:           1.6
+ * Version:           1.8
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Matteo Carassai
@@ -19,7 +19,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'WP_STATIC_EXPORTER_VERSION', '1.6' );
+define( 'WP_STATIC_EXPORTER_VERSION', '1.8' );
 define( 'WP_STATIC_EXPORTER_PATH', plugin_dir_path( __FILE__ ) );
 define( 'WP_STATIC_EXPORTER_URL', plugin_dir_url( __FILE__ ) );
 
